dbe62ec21e62560588a8179f2492404330f2268a
akykaan <kaanaky77@gmail.com>
2022-09-08 02:54:04 +0300

    node_module and vercel.json

---
+++ node_modules/@ardnt/vercel-python-wsgi/build-utils/index.js
@@ -0,0 +1,9 @@
+const log = require('./log.js');
+const pip = require('./pip.js');
+const python = require('./python.js');
+
+module.exports = {
+  log,
+  pip,
+  python,
+};
---
+++ node_modules/@ardnt/vercel-python-wsgi/vercel_python_wsgi/__init__.py
@@ -0,0 +1,1 @@
+from .handler import handler, vercel_handler # noqa
