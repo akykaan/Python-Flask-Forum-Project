1cd709fbea47d1f0ee49ce556a540472037d409d
akykaan <kaanaky77@gmail.com>
2022-09-08 16:17:41 +0300

    .

--- setting.py
+++ setting.py
@@ -6,8 +6,10 @@ STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 DATABASES = {
-    'default': {
-        'ENGINE': '/instance/flaskr.sqlite',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    #'default': {
+    #    'ENGINE': '/instance/flaskr.sqlite',
+    #    'NAME': BASE_DIR / 'db.sqlite3',
+    #}
 }
+
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
